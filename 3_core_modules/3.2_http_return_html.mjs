277ed2ebcdc111216c194c2fe598d5bea1e23397
Tiago Schwab EB <tiagoschwabbs@gmail.com>
2025-04-27 16:01:59 -0300

    Pequenas alterações

--- 3_core_modules/3.2_http_return_html.mjs
+++ 3_core_modules/3.2_http_return_html.mjs
@@ -1,10 +1,29 @@
-import { createServer } from 'node:http';
+import http from 'http';
+import fs from 'fs';
+import path from 'path';
 
-const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>Hello World<h1>');
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    const filePath = path.join(__dirname, 'index.html');
+    fs.readFile(filePath, (err, content) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Erro no servidor');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end('Página não encontrada');
+  }
 });
 
-server.listen(3000, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3000');
+server.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000');
 });
